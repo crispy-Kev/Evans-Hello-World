9cfe16e1b766a92fd1f47809a11ccf25a5cfb1b9
Evan Cornwell <theevancornwell@gmail.com>
2026-05-28 11:13:22 -0600

    Initial Commit

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+const person = {
+    name: "Lebron James",
+    age: 38,
+    occupation: "Basketball Player"
+};
+
+console.log(`My name is ${person.name}, I am ${person.age} years old, and I work as a ${person.occupation}.`);
